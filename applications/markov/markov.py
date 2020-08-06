8e2f8e4e5acc462439ff7ad770a78012a77ea5f9
Eddie Madrigal <edmadrigal@yahoo.com>
2020-08-05 22:26:05 -0700

    file refactored

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,29 @@
 import random
+from collections import defaultdict
 
 # Read in all the words in one go
-with open("input.txt") as f:
-    words = f.read()
+with open("/Lambda/git/cs-module-project-hash-tables/applications/markov/input.txt") as f:
+    words = f.read().split()
 
 # TODO: analyze which words can follow other words
 # Your code here
-
+word_dict = defaultdict(list)
+for word, next_word in zip(words, words[1:]):    #list two words at a time: the first word and then the following word
+    word_dict[word].append(next_word)   # create a dictionary and append the following word
 
 # TODO: construct 5 random sentences
 # Your code here
 
+# a = ["this", "is", "a", "sentence"]
+# print(a[1:])    # prints all words after the first one ==> ['is', 'a', 'sentence']
+# print(list(zip(a, a[1:])))  # create a list of combinations
+
+# print(word_dict["For"])
+
+startWord = ['For', 'The', 'But', 'Dinah', 'Well']
+for i in range(5):    
+    word = random.choice(startWord)
+    while not word.endswith("."):
+        print(word, end=' ')
+        word = random.choice(word_dict[word])
+    print(word)
